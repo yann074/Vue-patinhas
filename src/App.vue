441f63bb4cd2e7c2fded546f0be1de9c0e809b95
yann074 <yannsmith2907@gmail.com>
2024-02-19 20:58:46 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+//import { RouterLink, RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
+import HeaderView from './views/HeaderView.vue'
+</script>
+
+<template>
+  <header>
+    <HeaderView />
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/views/HeaderView.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <main>
+    </main>
+    <img :src="caminhodaFoto" alt="">
+
+    <h1></h1>
+</div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            caminhodaFoto: '/logo.png'
+        }
+    }
+}
+</script>
+<style></style>
